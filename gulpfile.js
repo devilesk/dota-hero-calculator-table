b155198e1378d3476320d9288ce48060de84d65f
devilesk <devilesk@oracle.localdomain>
2017-01-23 00:50:27 +0000

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,15 @@
 var gulp = require('gulp');
-var preprocess = require('gulp-preprocess');
-var replace = require('gulp-replace');
+var rollbar = require('gulp-rollbar');
 var git = require('git-rev-sync');
 var sourcemaps = require('gulp-sourcemaps');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var browserify = require('browserify');
-var gulpSequence = require('gulp-sequence');
-var del = require('del');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-clean-css');
-var imagemin = require('gulp-imagemin');
+var config = require('./config.json');
 
-gulp.task('css', function () {
-    return gulp.src([
-          'www/css/*.css'
-        ])
-        .pipe(concat('app.css'))
-        .pipe(minifyCSS())
-        .pipe(rename('app.min.' + git.short() + '.css'))
-        .pipe(gulp.dest('dist/css'))
-});
-
-gulp.task('copy-navbar', function () {
-    return gulp.src('/srv/www/dev.devilesk.com/dota2/.navbar.html')
-        .pipe(gulp.dest('www/'))
-});
-
-gulp.task('html', ['copy-navbar'], function () {
-    return gulp.src('www/index.html')
-        .pipe(preprocess({
-            context: {
-                NODE_ENV: 'production',
-                COMMIT_HASH: git.short()
-            }
-        })) //To set environment variables in-line 
-        .pipe(replace('bootstrap.css', 'bootstrap.min.css'))
-        .pipe(gulp.dest('dist/'))
-});
-
-gulp.task('image', function () {
-    return gulp.src('www/img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
-});
-
-gulp.task('bundle-prod', function () {
-    return browserify(['./www/js/main.js'], {debug:true})  // Pass browserify the entry point
-        .external('knockout')
-        .external('jquery')
-        .transform('brfs')
-        .transform('browserify-replace', {
-            replace: [
-                { from: /#DEV_BUILD/, to: new Date().toString() },
-                { from: /#code_version/, to: git.long() },
-                { from: /environment: 'development'/, to: "environment: 'production'" }
-            ]
-        })
-        .bundle()
-        .pipe(source('./www/js/main.js'))
-        .pipe(buffer())
-        .pipe(rename('bundle.' + git.short() + '.js'))
+gulp.task('rollbar:sourcemap', function () {
+    return gulp.src('dist/**/*.min.js')
         .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist/js/'))
+        .pipe(rollbar({
+          accessToken: config.rollbar.server_token,
+          version: git.long(),
+          sourceMappingURLPrefix: config.rollbar.sourceMappingURLPrefix
+        }))
 });
-
-gulp.task('bundle', function () {
-    return browserify('./www/js/main.js', {debug:true})  // Pass browserify the entry point
-        .external('knockout')
-        .external('jquery')
-        .transform('brfs')
-        .bundle()
-        .pipe(source('./www/js/main.js'))
-        .pipe(buffer())
-        .pipe(rename('bundle.js'))
-        .pipe(gulp.dest('./www/js/'))
-});
-
-gulp.task('clean', function () {
-    return del([
-        './dist/**/*'
-    ], {force: true});
-});
-
-gulp.task('clean-deploy', function () {
-    return del([
-        '/srv/www/devilesk.com/dota2/heroes/hero-stat-table/**/*'
-    ], {force: true});
-});
-
-gulp.task('deploy', function () {
-    return gulp.src([
-            'dist/**/*'
-        ])
-        .pipe(gulp.dest('/srv/www/devilesk.com/dota2/heroes/hero-stat-table'));
-});
-
-gulp.task('staging', gulpSequence('bundle-prod', ['css', 'html', 'image']));
-
-gulp.task('full-deploy', gulpSequence('staging', 'clean-deploy', 'deploy'));
---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+var config = require('../config.json');
+var path = require('path');
+var execSync = require('child_process').execSync;
+var del = require('del');
+
+var env = process.argv.indexOf('production') !== -1 ? 'production': 'development';
+console.log('env', env);
+
+var deployPath = config.deployPath[env];
+
+// clean and move to deploy directory
+var normalizedPath = path.normalize(deployPath);
+var paths = [
+    normalizedPath + '/**/*',
+    '!' + normalizedPath + '/save',
+    '!' + normalizedPath + '/save/*'
+]
+console.log(paths);
+del.sync(paths, {force: true});
+execSync('cp -r dist/* ' + normalizedPath);
